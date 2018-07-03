eb02195d2b19bf67f1c4120bc6f2b2ab7f82f2d4
adwaithcd <adwaithgowda@gmail.com>
2018-07-03 15:18:20 +0000

    Modifications for heroku deployment

--- zohoinvoice/__init__.py
+++ zohoinvoice/__init__.py
@@ -1,7 +1,7 @@
 import os
 DEV_ENV = os.environ.get("ENV")
 if DEV_ENV == "HEROKU":
-    YA_DEVELOPER_TOKEN = os.environ.get("YELLOWANT_DEVELOPER_TOKEN")
+    YA_DEVELOPER_TOKEN=os.environ.get("YELLOWANT_DEVELOPER_TOKEN")
 
     base = os.environ.get("HEROKU_APP_NAME")
 
